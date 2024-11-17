7ab52c3c868da95a99247c6584c5c6d70b458272
Gabriellbezerra <ackermanngabriell@gmail.com>
2024-11-17 18:58:31 -0300

    feat: Home page with dashboards and features.

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -5,12 +5,14 @@ import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 
 const TransactionsPage = async () => {
     const { userId } = await auth();
     if (!userId) {
         redirect("/login");
     }
+    const userCanAddTransaction = await canUserAddTransaction();
     const transactions = await db.transaction.findMany({
         where: {
             userId,
@@ -23,7 +25,7 @@ const TransactionsPage = async () => {
             {/* Título e Botão */}
             <div className="flex w-full items-center justify-between pl-6 pr-6">
                 <h1 className="font-bold text-2xl ">Transações</h1>
-                <AddTransactionButton userCanAddTransaction={"true"}/>
+                <AddTransactionButton userCanAddTransaction={userCanAddTransaction}/>
             </div>
             {/* Tabela */}
             <DataTable columns={transactionColumns} data={transactions}/>
