0bf3bb0aa438b91c46819ceee2ce037a422d0325
Gabriellbezerra <ackermanngabriell@gmail.com>
2024-11-17 16:22:42 -0300

    feat: transaction type badges now dont have hover effect.

--- app/transactions/_components/typeBadge.tsx
+++ app/transactions/_components/typeBadge.tsx
@@ -15,17 +15,18 @@ const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
       </Badge>
     );
   }
-  if (transaction.type === TransactionType.EXPENSE) {
+
+  else if (transaction.type === TransactionType.EXPENSE) {
     return (
-      <Badge className="font bold bg-danger bg-opacity-10 text-danger hover:bg-muted">
+      <Badge className="font bold bg-danger bg-opacity-10 text-danger hover:bg-danger hover:bg-opacity-10">
         <CircleIcon className="mr-2 fill-danger" size={10} />
         Despesa
       </Badge>
     );
   }
-  if (transaction.type === TransactionType.INVESTMENT){
+  else if (transaction.type === TransactionType.INVESTMENT){
     return (
-        <Badge className="font bold bg-white bg-opacity-10 text-white hover:bg-muted">
+        <Badge className="font bold bg-white bg-opacity-10 text-white hover:bg-white hover:bg-opacity-10">
           <CircleIcon className="mr-2 fill-white" size={10} />
           Investimento
         </Badge>
